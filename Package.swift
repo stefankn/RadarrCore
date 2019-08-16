c99589a8b3bf7b6e6f9d631f2a05878bab221d1a
Stefan Klein Nulent <skleinnulent@gmail.com>
2019-08-16 10:58:25 +0200

    Add tvOS compatibility

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "RadarrCore",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
